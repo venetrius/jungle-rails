7b288637a1a682c022f7ca47fad98ffc9a458e5d
venetrius <gergelygjuhasz@gmail.com>
2019-06-13 20:19:57 +0000

    add error listing to usr new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+    puts 'empty user'
+    puts @user
   end
 
 def create
-  user = User.new(user_params)
-  if user.save
+  @user = User.new(user_params)
+  if @user.save
     session[:user_id] = user.id
     redirect_to '/'
   else
-    redirect_to '/users/new'
+    render '/users/new'
   end
   
 end
