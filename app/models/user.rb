fbf17df49c25fb6a114832496adfab5bb031a5d0
venetrius <gergelygjuhasz@gmail.com>
2019-06-13 19:33:55 +0000

    add e-mail validation User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
 end
