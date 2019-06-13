6260bb97ff88ad410cf25d2470a5215f872f5e7c
venetrius <gergelygjuhasz@gmail.com>
2019-06-13 17:36:25 +0000

    save "new User" on register using "bcrypt"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,19 @@ class UsersController < ApplicationController
 
 def create
   @user = User.new(user_params)
-  render 'new'
+  if @user.save
+    #session[:user_id] = user.id
+    redirect_to '/'
+  else
+    redirect_to '/users/new'
+  end
+  
 end
 
 private
 
 def user_params
+  params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
+  get '/users', to: redirect('/')
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
