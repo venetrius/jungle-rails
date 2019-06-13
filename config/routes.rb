202c6acb1a60672358ca028e343c2f52435d2b11
venetrius <gergelygjuhasz@gmail.com>
2019-06-13 19:28:11 +0000

    wire in session

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
   get '/users', to: redirect('/')
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
