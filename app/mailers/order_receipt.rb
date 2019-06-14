12668e5a2b8622925220878992fcf7e6bf6e3e78
venetrius <gergelygjuhasz@gmail.com>
2019-06-14 22:03:33 +0000

    review order_receipt

--- app/mailers/order_receipt.rb
+++ app/mailers/order_receipt.rb
@@ -2,9 +2,9 @@ class OrderReceipt < ApplicationMailer
   default from: 'notifications@example.com'
  
   def order_placed order
-    @url  = 'http://example.com/login'
+    @url  = 'no-reply@jungle.com'
     @order = order
     @order_products = order.line_items.all.map {|item|  {product:item.product, quantity: item.quantity}}
-    mail(to: @order.email, subject: 'Your order has been placed!')
+    mail(to: @order.email, subject: "Order no.#{order.id} has been placed! ")
   end
 end
