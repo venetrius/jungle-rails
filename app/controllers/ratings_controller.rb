ec2ee0e931b39a61535d909a2cd79268b133548e
venetrius <gergelygjuhasz@gmail.com>
2019-06-14 19:12:34 +0000

    add "create rating" logic

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,7 +1,21 @@
 class RatingsController < ApplicationController
 
   def create
-    raise "Yay, I'm here!"
+    product_id = params.require(:product_id)
+    @product = Product.find(product_id)
+    @rating = @product.ratings.create(rating_params)
+    @rating.user = current_user
+    if @rating.save
+      redirect_to :back, notice: 'Review Added!'
+    else
+      render 'products/show'
+    end
+
   end  
 
+  private 
+
+  def rating_params
+    params.require(:rating).permit(:rating, :description)
+  end
 end
