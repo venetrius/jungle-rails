3485769be96a18212162144ae289e0f50bbe6518
venetrius <gergelygjuhasz@gmail.com>
2019-06-14 22:30:33 +0000

    show avr rating on Product#index

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -3,5 +3,10 @@ module ProductHelper
   def sold_out?(product)
     product.quantity == 0
   end
+
+  def average_rating(product)
+    rating_values = product.ratings.all.map{|rating| rating.rating}
+    rating_values.inject{ |sum, el| sum + el } / rating_values.size
+  end 
   
 end
