701cf06c4da40af0fcb8ac0efce5c019c68bf7da
venetrius <gergelygjuhasz@gmail.com>
2019-06-14 02:23:23 +0000

    add constraints to Rating

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,4 +2,8 @@ class Rating < ActiveRecord::Base
 
   belongs_to :product
 
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :rating, presence: true
+
 end
