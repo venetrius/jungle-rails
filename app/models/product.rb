be3fc976e3a20f98fbb55091240adcf772b3af6a
venetrius <gergelygjuhasz@gmail.com>
2019-06-14 02:05:06 +0000

    creating the Review model and table

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,8 @@ class Product < ActiveRecord::Base
   mount_uploader :image, ProductImageUploader
 
   belongs_to :category
+  has_many :rating
+
 
   validates :name, presence: true
   validates :price, presence: true
