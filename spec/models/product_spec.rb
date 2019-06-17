c877c1452622d8faf015b46d52dee11f281e5a12
venetrius <gergelygjuhasz@gmail.com>
2019-06-17 17:39:35 +0000

    product model testing

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  cat1 = Category.new({name: 'Apparel'})
+  subject {
+    described_class.new(name: "Sonic Screwdriver", price: 2000000,
+      quantity: 3, category: cat1)
+  }
+
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a name" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a price" do
+    subject.price = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a quantity" do
+    subject.quantity = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a category" do
+    subject.category = nil
+    expect(subject).to_not be_valid
+  end
 end
